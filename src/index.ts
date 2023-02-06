526f10ad3747f20a84b72513fe19056a35904906
matthewcaminiti <matthew.caminiti@mail.mcgill.ca>
2023-02-05 18:06:36 -0700

    First dump

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import {coolFunc} from './mything'
+
+const jah = (): number => {
+	console.log("jaaaaah HOT LOAD? JAH")
+	coolFunc()
+
+	return 0
+}
+
+jah()
---
+++ src/mything.ts
@@ -0,0 +1,3 @@
+export const coolFunc = () => {
+	console.log("coolest func with HOT LOADS? YESSS")
+}
